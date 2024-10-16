dcb77437d3bd4285573aa61ffb370c7b0b0d8087
erik <erik.cr42@gmail.com>
2024-10-16 17:56:14 -0300

    fixes path issue with github page

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,5 +6,14 @@ fetch(path)
     .then(response => response.text())
     .then(data => {
         document.getElementById("navbar-container").innerHTML = data;
+
+        // Ajusta os links da navbar
+        const links = document.querySelectorAll("#navbar-container a.nav-link");
+        links.forEach(link => {
+            if (window.location.pathname.includes("/pages/")) {
+                link.href = link.href.replace("index.html", "../index.html");
+                link.href = link.href.replace("pages/", "../pages/");
+            }
+        });
     })
     .catch(error => console.error("Erro ao carregar a navbar:", error));
